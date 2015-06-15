0a0e6fdac61ebad43bddf9135bbaceeb0101a085
speregil <s.gilparga@gmail.com>
2015-06-14 21:56:03 -0500

    Mago de oz de los datos de densidad.... odio JSON

--- va_course_2015-master/web_viewer/dino_server.py
+++ va_course_2015-master/web_viewer/dino_server.py
@@ -3,6 +3,7 @@ import tornado.web
 import os
 import pandas as pd
 import numpy as np
+import json
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
@@ -44,10 +45,35 @@ class DataHandler(tornado.web.RequestHandler):
         #guest_df_list = guest_df.to_dict("records")        
         #self.write({"array" :guest_df_list})
         
+        """
         df_checks_positions = df.loc[df["type"]=="check-in"].drop_duplicates(['X', 'Y']).ix[:,'X':]
         df_checks_list = df_checks_positions.to_dict("records")
         self.write({"array" :df_checks_list})
-
+        """
+        """
+        i = 0
+        data_list = []
+        df_checks_positions = df.loc[df["type"]=="check-in"].drop_duplicates(['X', 'Y']).ix[:,'X':]
+        while i < df_checks_positions.shape[0]:
+            df_checks_countA = df.loc[df["X"]==df_checks_positions['X'].iloc[i]]
+            df_checks_countB =  df_checks_countA.loc[df_checks_countA["Y"]==df_checks_positions['Y'].iloc[i]]
+            x = df_checks_countB['X'].iloc[0]
+            y = df_checks_countB['Y'].iloc[0]
+            density = df_checks_countB.count(axis=0).iloc[3]
+            data_dict = {'X': x, 'Y': y, 'density': density}
+            data_list.append(data_dict)
+            i+=1
+        df_density = pd.DataFrame.from_dict(data_list)
+        self.write({"array" :df_density})
+        """
+        data = pd.DataFrame(columns=('X', 'Y', 'count'))
+        x = 63
+        y = 99
+        cuenta = df['X'].loc[(df['X'] == x) & (df['Y'] == y) & (df["type"]=="check-in")].count()
+        data.loc[len(data)+1]=[x, y,cuenta] 
+        df_checks_list = data.to_dict("records")
+        self.write({"array" :df_checks_list})
+        
     def initialize(self, df):
         self.df = df[["X","Y","id","Timestamp","type"]]
 
--- va_course_2015-master/web_viewer/static/js/dino_map.js
+++ va_course_2015-master/web_viewer/static/js/dino_map.js
@@ -1,6 +1,7 @@
 /**
  *
  */
+ 
     var date_format = d3.time.format("%Y-%m-%d %H:%M:%S");
     var scale_x = d3.scale.linear();
     scale_x.domain([0, 100]);
@@ -23,21 +24,42 @@
 
     var svg = d3.select("#main_svg");
     var scatter = svg.append("g");
+	
+	/**
+	Variables "auxiliares"
+	**/
+	
+	densidad_viernes={ 
+     "X":[63,0,99,73,76,17,6,38,73,27,43,87,79,32,34,83,86,16,79,60,48,43,85,87,17,92,45,69,78,87,82,47,67,16,23,42,28,78,81,26,50,76], 
+     "Y":[99,67,77,84,88,43,43,90,79,15,78,81,89,33,68,88,44,49,87,37,87,56,86,63,67,81,24,44,48,48,80,11,37,66,54,37,66,37,77,59,57,22], 
+     "densidad":[2182,912,1074,4045,1787,10779,2346,8377,3736,8694,10074,2571,2270,9291,9940,1690,9957,2301,2793,2539,3842,2417,1857,4798,6909,1587,12456,11260,10371,5687,2442,8866,2693,9187,14027,2812,8412,3772,3279,2627,461,2634]
+	};
+	
+	densidad_sabado={
+	 "X":[99,63,0,83,16,60,47,17,78,69,79,28,78,73,73,87,32,16,42,48,79,92,85,38,67,87,43,34,45,76,81,87,27,6,17,82,23,86,43,26,50,76],	
+	 "Y":[77,99,67,88,66,37,11,43,48,44,87,66,37,84,79,63,33,49,37,87,89,81,86,90,37,81,56,68,24,88,77,48,15,43,67,80,54,44,78,59,57,22],
+     "densidad":[3280,7590,2523,2854,13884,4558,12419,16048,14961,16262,4277,14359,5490,6935,6549,7913,14035,3465,4469,5553,3811,2359,3027,12015,4992,4879,3636,16547,18993,2732,5397,8979,12982,3645,11602,3887,22526,14108,16464,3736,231,4487]	 
+	};
+	
+	densidad_domingo={
+	 "X":[0,99,63,17,82,79,87,81,23,16,48,32,87,60,76,47,43,73,79,28,86,34,45,38,27,26,69,16,87,17,78,43,50,83,85,73,78,67,42,92,6,76,82,26,78,47],	
+	 "Y":[67,77,99,67,80,89,81,77,54,66,87,33,63,37,88,11,56,79,87,66,44,68,24,90,15,59,44,49,48,43,48,78,57,88,86,84,37,37,37,81,43,22,80,59,37,11],
+     "densidad":[3577,4226,7441,14118,4628,4041,5856,6745,26347,16056,7292,11841,9118,5502,3488,15361,4556,8102,5455,17314,18066,20234,23625,16224,15361,4600,18701,4125,10416,18309,16992,20674,306,3222,3221,7584,6366,5726,5975,3226,4453,4197,72,78,10,6]	 
+	};
 
 	function draw_checkins(array){
 		console.log("Llego la información");
-		console.log("Tamanio de la informacion: " + array.length);
-	}
-	
-    function draw_guest(array) {
+		console.log(densidad_domingo.densidad[32])
+			
     // parse times
         array.forEach(function (e, i, a) {
             e.time = date_format.parse(e.Timestamp);
         });
 
 
-        function draw_point(above_lim) {
-            scatter.selectAll(".dot").data(array.slice(0, above_lim)).enter().append("circle").attr("class", "dot")
+        function draw_ckeckinPoints(above_lim) {
+          //con enter() solo lee los datos nuevos y los viejos los deja igual. Si uso el attr() ahí si actualiza los existentes y exit().remove() cuando son menos datos y quiero que borre los restantes
+            scatter.selectAll(".checkin").data(array).enter().append("circle").attr("class", "checkin")
                     .attr("r", 6)
                     .attr("cx", function (d) {
                         return scale_x(d.X);
@@ -45,31 +67,23 @@
                     .attr("cy", function (d) {
                         return scale_y(d.Y);
                     })
-                    .attr("fill", function (d) {
+                    /*.attr("fill", function (d) {
                         return time_scale(d.time);
-                    })
-                    .classed("checkin",function(d){return d.type == "check-in";})
+                    })*/
+					.attr("fill", "yellow")
+                    //.classed("checkin",function(d){return d.type == "check-in";})
                     .append("title").text(function(d){return d.Timestamp});
         }
+		draw_ckeckinPoints(array);
+        d3.select("#show_guest_button").attr("disabled", null);
 
-        var i = 0;
-        d3.timer(function () {
-            d3.select("#clock").text(array[i].time.toLocaleTimeString());
-            draw_point(i);
-            i += 1;
-            if (i >= array.length) {
-                console.log("done");
-                d3.select("#show_guest_button").attr("disabled", null);
-                return true;
-            }
-            return false;
-        });
-
-    }
-
+    
+	}
+	
+   
 
     function read_guest() {
-        scatter.selectAll(".dot").remove();
+        scatter.selectAll(".checkin").remove();
         console.log("reading guest");
         d3.select("#show_guest_button").attr("disabled", true);
         d3.json("data", function (e, d) {
